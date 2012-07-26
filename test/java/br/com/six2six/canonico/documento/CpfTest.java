8c5af1b31ca20e3e4a4e0d8bc8cdeea1741cac15
Bjornn Borg <bjornnborg@gmail.com>
2012-07-26 02:39:18 -0300

    testes para cpf invalidos

--- test/java/br/com/six2six/canonico/documento/CpfTest.java
+++ test/java/br/com/six2six/canonico/documento/CpfTest.java
@@ -30,4 +30,17 @@ public class CpfTest {
 		Assert.assertEquals("Valor inválido", "43132717835", cpf.getValue());
 	}
 	
+	@Test(expected=DocumentoInvalido.class)
+	public void cpfInvalidoNaoLenienteDeveGerarErro() {
+		new Cpf("13911708301");
+	}
+	
+	@Test
+	public void cpfInvalidoNaoLenienteNaoPodeSeDizerValido() {
+		Cpf cpf = new Cpf("13911708301", true);
+		Assert.assertFalse("Cpf inválido", cpf.isValido());
+		Assert.assertEquals("Formatação inválida", "139.117.083-01", cpf.getDisplayValue());
+		Assert.assertEquals("Valor inválido", "13911708301", cpf.getValue());
+	}	
+	
 }
